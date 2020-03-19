7604bdecaa39a45695841cc438d4b09917c88262
yennanliu <f339339@gmail.com>
2020-03-19 17:13:12 +0800

    backup sql (movie_rented_2NF.sql)

--- de100days/day_19/movie_rented_2NF.sql
+++ de100days/day_19/movie_rented_2NF.sql
@@ -1,4 +1,5 @@
 -- https://stackoverflow.com/questions/25969/insert-into-values-select-from
+-- https://www.sqlitetutorial.net/sqlite-foreign-key/
 -- create member table with primary key, then insert data from movie_rented_1NF
 -- sqlite 
 
@@ -9,10 +10,33 @@ physical_address TEXT,
 saluation TEXT
 );
 
+CREATE TABLE movie_rented_list (
+member_id INTEGER,
+movie_rented TEXT,
+    FOREIGN KEY (member_id)
+    REFERENCES member(member_id)
+);
+
 INSERT INTO member (full_names, physical_address, saluation )
-select 
-DISTINCT
+SELECT DISTINCT
 full_names AS full_names, 
 physical_address AS physical_address,
 saluation AS saluation
 FROM  movie_rented_1NF;
+
+-- INSERT into movie_rented_list (member_id, movie_rented)
+-- SELECT DISTINCT 
+-- member.member_id,
+-- movie_rented_1NF.movie_rented 
+-- FROM movie_rented_1NF
+-- LEFT JOIN 
+-- member
+-- ON movie_rented_1NF.full_names = member.full_names;
+
+-- SELECT 
+-- member.member_id,
+-- movie_rented.movie_rented
+-- from member
+-- join 
+-- movie_rented
+-- on member.member_id = movie_rented.member_id;
